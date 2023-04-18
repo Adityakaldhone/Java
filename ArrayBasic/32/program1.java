24606c9ad500a43c05f38e24f0d509d8667e26ec
adityakaldhone1357@gmail.com <adityakaldhone1357@gmail.com>
2023-04-18 16:44:05 +0530

    ArrayBasic

---
+++ ArrayBasic/32/program1.java
@@ -0,0 +1,12 @@
+class Demo{
+
+	int a = 10;
+	public static void main(String[]args){
+	
+		int b = 20;
+		System.out.println(b);
+		System.out.println(a);//error: non static variable 'a' cannot be referenced from static context
+
+
+	}
+}
---
+++ ArrayBasic/32/program2.java
@@ -0,0 +1,8 @@
+class ArrayDemo{
+
+	public static void main(String[]args){
+	
+		int arr1[] = new int[5];
+		int arr2[] = int[]; // array dimensions missing
+	}
+}
