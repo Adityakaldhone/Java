e282890f56fbf34cc0bc3a98f58a8fa9230bb302
Adityakaldhone <adityakaldhone1357@gmail.com>
2023-06-02 12:56:58 +0530

    String

--- classcodes/String/program1.java
+++ classcodes/String/program1.java
@@ -11,6 +11,18 @@ class StringDemo{
 		String str3 = "Core2Web"; // String constant pool
 		String str4 = new String("Core2Web");//Heap
 		
+
+		String str5 = "Shashi";
+
+		System.out.println(str5);
+		
+		System.out.println("identity hashcode of str5 = " +System.identityHashCode(str5));
+
+		str5 = "bagal";
+
+		System.out.println(str5);
+		System.out.println("identity hashcode of str5 after assigning = " +System.identityHashCode(str5));
+		
 		System.out.println(System.identityHashCode(str3));
 		System.out.println(System.identityHashCode(str4));
 	}
