8edc6ecf8795240dba927d14ece1087b869e683a
Adityakaldhone <adityakaldhone1357@gmail.com>
2023-08-09 16:16:49 +0530

    Enumeration cursor

---
+++ classcodes/Collection/86/progra1.java
@@ -0,0 +1,22 @@
+//Enumeration
+import java.util.*;
+
+class EnumerationCursor {
+
+	public static void main(String[]args) {
+	
+		Vector v = new Vector();
+		v.addElement(10);
+		v.addElement(20);
+		v.addElement(30);
+		v.addElement(40);
+
+		Enumeration cursor = v.elements();
+		System.out.println(cursor.getClass().getName());
+		while(cursor.hasMoreElements()) {
+		
+			System.out.println(cursor.nextElement());
+		}
+		System.out.println("Element at indext 3 = " + v.elementAt(2));
+	}
+}
